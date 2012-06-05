0828b3a1e7a6b9920871f164f049df29ef4008a5
Robert Bruggner <rbruggner@gmail.com>
2012-06-05 15:13:02 -0700

    New extract IMD stream functions.

--- R/write.R
+++ R/write.R
@@ -31,3 +31,63 @@ cytofCore.write.FCS <- function(x, filename, what="numeric") {
     }
     write.FCS(x, filename, what)
 }
+
+cytofCore.write.IMD.stream = function(imdFile,confFile,windowSize=10000,startTime=0,endTime=NULL){
+	
+	timeConstant = 1/76800
+	continue=T
+	counter=0;
+	first=T
+	
+	#FILL THIS OUT
+	num_pushes <- as.integer(file.info(imdFile)$size / nrow(cytofCore.read.conf(confFile)) / 4)
+	max_time = (num_pushes-1)*timeConstant*1000
+	if (startTime>max_time){
+		stop(paste("Specified startTime is greater than IMD max time of",max_time))
+	}
+	if (!is.null(endTime)&&(endTime>max_time)){
+		stop(paste("Specified endTime is greater than IMD max time of",max_time,". Leave endTime as NULL (default) to read to end of file."))
+	}
+	
+	while (continue){
+		windowStartTime = (counter)*(windowSize)*timeConstant*1000
+		windowEndTime = (counter*windowSize+windowSize-1)*timeConstant*1000
+		windowTime = seq(from=windowStartTime,to=windowEndTime,by=timeConstant*1000)
+		#print(paste(windowStartTime,windowEndTime))
+		windowKeep=F
+		if (any(windowTime>=startTime)){
+			windowKeep = windowTime>=startTime
+		}
+		
+		if (!is.null(endTime)){
+			windowKeep = windowKeep & (windowTime<=endTime)
+			if (windowStartTime>endTime){
+				continue=F	
+			}
+		}
+		
+		
+		if (sum(windowKeep)>0){
+			startPush=counter*windowSize
+			if ((startPush+windowSize)>num_pushes){
+				pushes=cytofCore.read.imd(file=imdFile,conf=confFile,start_push=startPush)$dual
+				windowTime = windowTime[1:nrow(pushes)]
+				windowKeep = windowKeep[1:nrow(pushes)]
+				continue=F
+			} else {
+				pushes = cytofCore.read.imd(file=imdFile,conf=confFile,start_push=startPush,num_pushes=windowSize)$dual
+			}
+			pushes = cbind(time=sprintf("%.3f",windowTime[windowKeep]),pushes[windowKeep,])
+			print(paste("Writing",nrow(pushes),"pushes"))
+			if (first){
+				write.table(pushes,file=paste(imdFile,".txt",sep=""),quote=F,row.names=F,col.names=T,append=F)
+				first=F
+			} else {
+				write.table(pushes,file=paste(imdFile,".txt",sep=""),quote=F,row.names=F,col.names=F,append=T)	
+			}
+		}
+		
+		counter=counter+1;
+		
+	}
+}
