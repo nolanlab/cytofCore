eb88f7a6a72186904bc5397ac65d73488550a0e1
rbruggner <rbruggner@gmail.com>
2013-09-29 16:34:54 -0700

    Adding compensation utility functions.

---
+++ R/compensate.R
@@ -0,0 +1,28 @@
+applyCompensation = function(flowFrame,compMatrix){
+  return(compensate(flowFrame,compMatrix))
+}
+
+cytofCore.applyFileCompensation = function(inputFCSFile,inputCompFile,outputFile,row.names=1,skip=0){
+  flowFrame = read.FCS(inputFCSFile)
+  compMatrix = read.csv(inputCompFile,check.names=F,header=T,row.names=row.names,skip=skip)
+  compedFrame = applyCompensation(flowFrame,compMatrix)
+  write.FCS(compedFrame,filename=outputFile)
+}
+
+cytofCore.compensateDirectory = function(inputFCSDirectory,inputCompDirectory,outputDirectory,fcsFiles,compFiles){
+  if ((length(compFiles)=!1)||(length(compFiles)!=length(fcsFiles))){
+    stop("Must provide a single comp file or a comp file for each fcs file.")
+  }
+  if (length(compFiles)==1){
+    compFiles = rep(compFile,length(fcsFiles))
+  }
+  for (i in 1:length(fcsFiles)){
+    fcsFile = fcsFiles[i]
+    compFile = compFiles[i]
+    print("Comping",fcsFile,"with",compFile)
+    cytofCore.applyFileCompensation(inputFCSFile=file.path(inputFCSDirectory,fcsFile),
+                                    inputCompFile=file.path(inputCompDirectory,compFile),
+                                    outputFile=file.path(outputDirectory,fcsFile))
+  }
+}
+
