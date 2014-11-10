eb3246e9446dbd551ba3ea3e7bf77b1427350c7c
Rachel Finck <rachel.finck@gmail.com>
2014-11-09 16:06:35 -0800

    Adding reader for rd8 files.

--- R/read.R
+++ R/read.R
@@ -1,5 +1,5 @@
 cytofCore.read.conf <- function(file) {
-    read.table(file,header=TRUE)
+  read.table(file,header=TRUE)
 }
 
 cytofCore.read.imd.xml <- function(file) {
@@ -39,6 +39,7 @@ cytofCore.read.imd.xml <- function(file) {
   for (metal in analyteList) {
     analytes=rbind(analytes,c(metal$Mass,metal$Description,metal$MassSymbol ))
   }
+  
   analytes[,3]= paste(analytes[,3],as.character(round(as.numeric(analytes[,1]))),sep="")
   
   colnames(analytes)=c("mass","description","channel")
@@ -52,94 +53,145 @@ cytofCore.read.imd.xml <- function(file) {
   colnames(dualCalibration)=c("mass","slope","intercept")
   
   imd.xml=list()
-  imd.xml$analytes=analytes
-  imd.xml$dualCalibration=dualCalibration
+  imd.xml$analytes=data.frame(analytes)
+  imd.xml$dualCalibration=data.frame(dualCalibration)
   
   return(imd.xml)
   
 }
 
-cytofCore.read.imd <- function(file, analytes=NULL, conf=NULL, pulse_thresh=3.0, start_push=0, num_pushes=NULL) {
-    
-    
-    
-    # change to allow input analytes for when imd is missing tail
-    if (!is.null(analytes)) {
-		num_analytes <- length(analytes)
-    } else if (!is.null(conf)) {
-		if (is.character(conf) && file.exists(conf)) {
-			conf <- cytofCore.read.conf(conf)
-		}
-		num_analytes <- nrow(conf)
-		analytes <- paste(round(conf$Mass),conf$Symbol,sep="")
-    } else {
-      # Change this to read xml from tail of IMD file.
-		stop("One of analytes or conf must be specified to know how many analytes are present")
+cytofCore.read.imd <- function(file, conf=NULL, pulse_thresh=1.0, start_push=0, num_pushes=2^16) {
+  
+  # change to allow input analytes for when imd is missing tail
+  if (!is.null(conf)) {
+    if (is.character(conf) && file.exists(conf)) {
+      conf <- cytofCore.read.conf(conf)
     }
-
-    if (is.character(file)) {
-		if (is.null(num_pushes) && !is.na(file.info(file)$size)) {
-			# File size should be multiple of 2 * # analytes * 2 bytes
-			num_pushes <- as.integer(file.info(file)$size / num_analytes / 4)
-		}
-		file <- file(file, "rb")
-		on.exit(close(file))
-    }
-    if (!inherits(file, "connection")) {
-		stop("'file' must be a character string or connection")
-    }
-    if (!isOpen(file, "rb")) {
-		open(file, "rb")
-		on.exit(close(file))
-    }
-
-    # Read the IMD file in chunks
     
-	current_push <- 0
-	if (start_push > 0) {
-		if (!isSeekable(file)) {
-			stop("Cannot seek in specified file or connection")
-		}
-		# Each analyte consumes 4 bytes, 2 each for intensity and pulse values
-		seek(file, where=start_push*num_analytes*4, origin="current")
+    analytes <- paste(conf$Symbol,round(conf$Mass),sep="")
+    slopes=conf$Slope
+    intercepts=conf$Intercept
+  } else {
+    xml=cytofCore.read.imd.xml(file)
+    analytes=xml$analytes$description
+    masses=as.numeric(as.character(xml$analytes$mass))
+    slopes=approx(xml$dualCalibration$mass,xml$dualCalibration$slope,masses,method="linear",rule=2)$y
+    intercepts=approx(xml$dualCalibration$mass,xml$dualCalibration$intercept,masses,method="linear",rule=2)$y
+  } 
+  
+  num_analytes <- length(analytes)
+  
+  if (is.character(file)) {
+    # 		if (is.null(num_pushes) && !is.na(file.info(file)$size)) {
+    # 			# File size should be multiple of 2 * # analytes * 2 bytes
+    # 			num_pushes <- as.integer(file.info(file)$size / num_analytes / 4)
+    # 		}
+    file <- file(file, "rb")
+    on.exit(close(file))
   }
-      
-	while (is.null(num_pushes) || current_push < num_pushes) { 
-		desired_n <- ifelse(is.null(num_pushes), 2^16, num_pushes-current_push) * num_analytes * 2
-		if (exists("N")) {
-			before_n <- length(N)
-			N <- c(N,readBin(file, integer(), size=2, n=desired_n, signed=FALSE))  # Records are 16-bit unsigned integers
-			actual_n <- length(N)-before_n
-		} else {
-			N <- readBin(file, integer(), size=2, n=desired_n, signed=FALSE)  # Records are 16-bit unsigned integers
-			actual_n <- length(N)
-		}
-                
-		if (actual_n < desired_n) {
-            break;
-		}
-		current_push <- as.integer(length(N) / num_analytes / 2)
+  if (!inherits(file, "connection")) {
+    stop("'file' must be a character string or connection")
+  }
+  if (!isOpen(file, "rb")) {
+    open(file, "rb")
+    on.exit(close(file))
   }
   
-    I_cols <- seq(from=1, by=2, length.out=num_analytes)
-    P_cols <- seq(from=2, by=2, length.out=num_analytes)  
-    IP <- matrix(N,ncol=2*num_analytes,byrow=TRUE) 
-    
-    if (is.null(conf)) {
-		l <- list(intensity=IP[,I_cols],pulse=IP[,P_cols])
-		colnames(l$intensity) <- analytes
-		colnames(l$pulse)     <- analytes
-		return(invisible(l))
-    } else {
-		# If you supplied conf file, we assume you only want the dual counts
-		D <- c()
-		for (i in 1:num_analytes) {
-			d <- round(IP[,I_cols[i]]*conf$Slope[i]+conf$Intercept[i])
-			use_pulse = d < pulse_thresh
-			d[use_pulse] <- pmax(d[use_pulse],IP[use_pulse,P_cols[i]])
-			D <- cbind(D, d)
-		}
-		colnames(D) <- analytes
-		return(invisible(list(dual=D)))
+  # Read the IMD file in chunks
+  
+  current_push <- 0
+  if (start_push > 0) {
+    if (!isSeekable(file)) {
+      stop("Cannot seek in specified file or connection")
     }
+    # Each analyte consumes 4 bytes, 2 each for intensity and pulse values
+    seek(file, where=start_push*num_analytes*4, origin="current")
+  } 
+  
+#   while (is.null(num_pushes) || current_push < num_pushes) { 
+#     desired_n <- ifelse(is.null(num_pushes), 2^16, num_pushes-current_push) * num_analytes * 2
+#     if (exists("N")) {
+#       before_n <- length(N)
+#       N <- c(N,readBin(file, integer(), size=2, n=desired_n, signed=FALSE))  # Records are 16-bit unsigned integers
+#       actual_n <- length(N)-before_n
+#     } else {
+#       N <- readBin(file, integer(), size=2, n=desired_n, signed=FALSE)  # Records are 16-bit unsigned integers
+#       actual_n <- length(N)
+#     }
+#     
+#     if (actual_n < desired_n) {
+#       break;
+#     }
+#     current_push <- as.integer(length(N) / num_analytes / 2)
+#   }
+
+  #simplifying: just read in num_pushes number of pushes
+  N=readBin(file,integer(),size=2,n=num_pushes*2*num_analytes,signed=F)
+  
+  I_cols <- seq(from=1, by=2, length.out=num_analytes)
+  P_cols <- seq(from=2, by=2, length.out=num_analytes)  
+  IP <- matrix(N,ncol=2*num_analytes,byrow=TRUE) 
+  
+  # 20140709 updated dual conditions to those used by CyTOF software
+  D <- c()
+  for (i in 1:num_analytes) {
+    d <- round(IP[,I_cols[i]]*slopes[i]+intercepts[i])
+    use_pulse = d < IP[,P_cols[i]] & IP[,P_cols[i]]<= pulse_thresh 
+    d[use_pulse] <- IP[use_pulse,P_cols[i]]
+    D <- cbind(D, d)
+  }
+  colnames(D) <- analytes
+  
+  # Changing from earlier version and calculating I and P even if conf supplied     
+  l <- list(intensity=IP[,I_cols],pulse=IP[,P_cols],dual=D)
+  colnames(l$intensity) <- analytes
+  colnames(l$pulse)     <- analytes
+  
+  return(l)
+}
+
+cytofCore.read.rd8 <- function(file, segmentSize=3200,  massCalibration=NULL, cytofType="cytof1", start_push=0, num_pushes=2^8) {
+  #returns a list with raw intensities where each row is a push and each column is the tof trace of that push.
+  #including the optional input argument of the massCalibration adds the massPeak locations to the list
+  #the massCalibration parameter is a list with names "time", "mass", and "triggerDelay". 
+  #"time" and "mass" are 2-element vectors whose values are visible in the CyTOF software mass calibration window
+  # Example:  massCalibration=list(mass=c(132.905,192.963),time=c(9597,11537),triggerDelay=8416)
+
+  
+
+  if (is.character(file)) {
+    file <- file(file, "rb")
+    on.exit(close(file))
+  }
+  
+  results=list()
+  
+  # cytof1 has 1 uint8 value per intensity sample, cytof2 has 2 uint8 values for each sample
+  if (cytofType=="cytof1") {
+    seek(file, where=start_push*segmentSize, origin="current")
+    N=readBin(file,integer(),size=1,n=num_pushes*segmentSize,signed=F)
+    intensity <- matrix(N,ncol=segmentSize,byrow=TRUE,dimnames=list(as.character(start_push:(start_push+num_pushes-1)))) 
+  } else if (cytofType=="cytof2") {
+    board1_cols <- seq(from=1, by=2, length.out=segmentSize)
+    board2_cols <- seq(from=2, by=2, length.out=segmentSize)  
+    bothBoards <- matrix(N,ncol=2*segmentSize,byrow=TRUE,dimnames=list(as.character(start_push:(start_push+num_pushes-1))))  
+    intensity=list(board1=bothBoards[,board1_cols], board2=bothBoards[,board2_cols] )
+  }
+
+  results$intensity=intensity  
+  
+  #calibrate the mass windows to the raw data
+  if (!is.null(massCalibration)) { 
+    A=(massCalibration$time[1]-massCalibration$time[2])/(sqrt(massCalibration$mass[1]) - sqrt(massCalibration$mass[2]))
+    T0=massCalibration$time[1]-A*sqrt(massCalibration$mass[1])
+    masses=102:195
+    massPeaks=T0+A*sqrt(masses) - massCalibration$triggerDelay;
+    names(massPeaks)=as.character(masses)
+  }
+  
+  # the mass peaks correspond to the columns of the intensities
+   results$massPeaks=massPeaks 
+  
+  return(results)
+    
 }
